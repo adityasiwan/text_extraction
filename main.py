a1b0d196834417003420174b4ff0035ca4782520
Aditya Harsh <adityasiwan@live.com>
2018-10-03 22:54:28 +0530

    Add: Code for cosine similarity between two document

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import cv2
 import numpy as np
 import pytesseract
 from PIL import Image
+from metric import final
 
 path = "test_data/"
 output = "result/"
 # Read image with opencv
-img = cv2.imread(path + 'image.jpg')
+img = cv2.imread(path + 'image1.jpg')
 
 # Convert to gray
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Remove noise
-# kernel = np.ones((1, 1), np.uint8)
-# img = cv2.dilate(img, kernel, iterations=1)
-# img = cv2.erode(img, kernel, iterations=1)
+kernel = np.ones((1, 1), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+img = cv2.erode(img, kernel, iterations=1)
 
 # Write image after removed noise
 cv2.imwrite(output + "removed_noise.png", img)
 
 # Recognize text with tesseract for python
-result = pytesseract.image_to_string(Image.open(output + "removed_noise.png"))
+result = pytesseract.image_to_string(Image.open(output + "removed_noise.png"), config = ('-l eng --oem 1 --psm 3'))
 
 
 print(result)
