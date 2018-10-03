d9c6e8b36472fc6881f611726654348fdda66399
Aditya Harsh <adityasiwan@live.com>
2018-10-04 00:14:23 +0530

    Add: script for dependencies installation

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:alex-p/tesseract-ocr
+sudo apt-get update
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
+sudo pip install pytesseract
+pip install -r requirements.txt
